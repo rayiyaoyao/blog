d5195fab905b761415165bb4be2f3d03d7b8f12a
rayiyaoyao <357837043@qq.com>
2016-11-18 19:27:18 +0800

    adding media & bug fixing

--- blog/settings.py
+++ blog/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "midea_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 
 
